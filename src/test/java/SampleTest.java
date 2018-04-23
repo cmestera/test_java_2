2ad270093c24f8390ff0b7796f333818d5595a9d
cmestera <ciprian.mester@aurea.com>
2018-04-24 00:05:04 +0300

    Add gradle build with a basic Junit test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void shouldDoABasicTest() throws Exception {
+        System.out.println("In the simple test");
+        assertTrue("My test condition",false);
+    }
+}
