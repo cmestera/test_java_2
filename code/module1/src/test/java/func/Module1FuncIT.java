c1de088c46c26f8bf7a91ae874e3cccef731cf27
cmestera <ciprian.mester@aurea.com>
2018-06-23 12:21:00 +0300

    Add failsafe IT for 1 module

--- code/module1/src/test/java/func/Module1FuncIT.java
+++ code/module1/src/test/java/func/Module1FuncIT.java
@@ -1,9 +1,11 @@
+package func;
 import module1.Module1Class;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class Module1ClassTest {
+public class Module1FuncIT {
     @Test
     public void shouldDoABasicTest() throws Exception {
         assertTrue("My test condition", Module1Class.testedMethod());
