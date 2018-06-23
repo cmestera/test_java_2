870acf8daf0001cb25b5d21abd065d0872f3e874
cmestera <ciprian.mester@aurea.com>
2018-06-23 12:26:42 +0300

    Add failsafe IT for 1 module

---
+++ code/module1/src/main/java/module1/Module1Class.java
@@ -0,0 +1,13 @@
+package module1;
+
+public class Module1Class {
+
+    public static boolean testedMethod() {
+        return true;
+    }
+
+    public static boolean untestedMethod() {
+        return true;
+    }
+
+}
